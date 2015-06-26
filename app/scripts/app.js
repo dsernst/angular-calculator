a778baeb44e3214edd5b37289f4ed94ccc89d8b0
David Ernst <git@dsernst.com>
2015-06-26 09:41:44 -0700

    yo angular

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name calculatorApp
+ * @description
+ * # calculatorApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('calculatorApp', [
+    'ngTouch'
+  ]);
