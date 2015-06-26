a098909285b2197483a44c9392494ad9500f339c
David Ernst <git@dsernst.com>
2015-06-26 13:51:17 -0700

    add button functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,6 +9,52 @@
  */
 angular.module('calculatorApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.result = 0;
-    $scope.display = $scope.result;
+    $scope.currentOperator = null;
+    $scope.currentValue = 0;
+    $scope.display = '0';
+
+    $scope.pressClear = function () {
+      $scope.currentValue = 0;
+      $scope.display = $scope.currentValue.toString();
+    };
+
+    $scope.pressNumber = function (value) {
+      if ($scope.display === '0') {
+        $scope.display = '';
+      }
+      $scope.display = $scope.display + value;
+    };
+
+    $scope.pressEquals = function () {
+      var secondArgument = +$scope.display;
+
+      if ($scope.currentOperator === 'divide') {
+        $scope.display = ($scope.currentValue / secondArgument).toString();
+      } else if ($scope.currentOperator === 'times') {
+        $scope.display = ($scope.currentValue * secondArgument).toString();
+      } else if ($scope.currentOperator === 'minus') {
+        $scope.display = ($scope.currentValue - secondArgument).toString();
+      } else if ($scope.currentOperator === 'plus') {
+        $scope.display = ($scope.currentValue + secondArgument).toString();
+      }
+    };
+
+    $scope.pressOperator = function (operator) {
+      $scope.currentOperator = operator;
+      $scope.currentValue = +$scope.display;
+      $scope.display = '0';
+    };
+
+    // $scope.pressPercent = function () {
+    //   $scope.display = (+$scope.display / 100).toString();
+    // };
+
+    // $scope.pressInverse = function () {
+    //   $scope.display = (+$scope.display * -1).toString();
+    // };
+
+    $scope.pressModify = function (modifier) {
+      $scope.display = (+$scope.display * modifier).toString();
+    };
+
   });
